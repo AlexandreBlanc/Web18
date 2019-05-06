15b51acc6dccb2ed880663f10c67e50982f04af0
Alexandre Blanc <alexandre.blanc@viacesi.fr>
2019-05-06 15:05:10 +0200

    Différent section du site

--- index.php
+++ index.php
@@ -1,8 +1,21 @@
 <?php
 
-$version = 7;
-$message = '<h1>Je fais du PHP' . $version . '</h1>'
-echo $message;
+$resultat = '<ul>';
+for ($i = 1 ; $i <= 100 ; $i++){
+    $resultat .= '<li>' . $i . '</li>';
+}
 
-// Utilisation
-define('TVATxPlein', 0.2);
+$resultat .= "</ul>"; 
+
+
+$resultat = '<ul>';
+for ($i = -12328 ; $i <= 23456 ; $i += 7){
+    if ($i % 5 == 0) {
+    $resultat .= '<li>' . $i . '</li>';
+    }
+}
+
+$resultat .= "</ul>";
+
+echo $resultat;
+
