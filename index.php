edf95f859745118d100f0a87e3c6a52be2c0f08d
Alexandre Blanc <alexandre.blanc@viacesi.fr>
2019-05-06 16:08:29 +0200

    Includession

--- index.php
+++ index.php
@@ -1,21 +1,6 @@
 <?php
 
-$resultat = '<ul>';
-for ($i = 1 ; $i <= 100 ; $i++){
-    $resultat .= '<li>' . $i . '</li>';
-}
-
-$resultat .= "</ul>"; 
-
-
-$resultat = '<ul>';
-for ($i = -12328 ; $i <= 23456 ; $i += 7){
-    if ($i % 5 == 0) {
-    $resultat .= '<li>' . $i . '</li>';
-    }
-}
-
-$resultat .= "</ul>";
-
-echo $resultat;
+require_once './includes/header.php';
+require_once './includes/content.php';
+require_once './includes/footer.php';
 
